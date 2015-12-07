12d25b8ed325a22a92f98497cd4a404cea902e49
i309511 <harrifeng@gmail.com>
2015-12-06 23:31:54 -0800

    before chapter 3

---
+++ extra/ex08.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "github.com/harrifeng/gopl.io/ch2/tempconv0"
+
+func main() {
+	var c tempconv.Celsius
+	fmt.Println(c)
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// 0°C											  //
+////////////////////////////////////////////////////
