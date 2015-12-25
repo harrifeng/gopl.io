466eb3490d2360ba71d784cce6771ab6792d1717
Harri Feng <harrifeng@gmail.com>
2015-12-25 14:56:23 +0800

    before 4.3 map

---
+++ extra/ex30.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
+
+func main() {
+	data := []string{"one", "", "three"}
+	// usually write
+	// data = nonempty(data)
+	fmt.Printf("%q\n", nonempty(data))
+	fmt.Printf("%q\n", data)
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// ["one" "three"]								  //
+// ["one" "three" "three"]						  //
+////////////////////////////////////////////////////
---
+++ extra/ex32.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func remove(slice []int, i int) []int {
+	slice[i] = slice[len(slice)-1]
+	return slice[:len(slice)-1]
+}
+
+func main() {
+	s := []int{5, 6, 7, 8, 9}
+	fmt.Println(remove(s, 2))
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// [5 6 9 8]									  //
+////////////////////////////////////////////////////
